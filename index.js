3fb64909fc2c1079fea9f763a0abc62ed96ee9af
mubarak <mubarakcali24@gmail.com>
2023-04-30 14:01:31 -0700

    render shapes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const {Triangle, Circle, Square} = require("./lib/shapes");
 
 const prompt = () => {
     return inquirer.prompt([
@@ -6,37 +8,55 @@ const prompt = () => {
             type: 'input',
             name: 'Text',
             message: 'Text can only be 3 characters',
+            validate: (text) => {
+                text.length <= 3 ||
+                "shouldn't contain more than 3 characters"
+            }
         },
 
         {
             type: 'input',
-            name: 'Color',
+            name: 'Textcolor',
             message: 'Enter a text color keyword',
         },
 
         {
-            type: 'input',
+            type: 'list',
             name: 'Shape',
             message: 'Choose from a list of shapes',
+            choices: ["triangle", "circle", "square"],
         },
 
         {
             type: 'input',
-            name: 'Color',
+            name: 'Shapecolor',
             message: 'Enter a shape color keyword',
         },
     ]);
 };
 
-const init = () => { 
+const init = () => {
     prompt()
-    .then((answers) => fs.writeFile('logo.svg', writeToFile(answers),
-    (err) => {
-        if (err)
-          console.log(err);
-        else {
-          console.log("Generated logo.svg\n");
+        .then((answers) => {
+            let newShape;
+            if (answers.Shape === "triangle") {
+                newShape = new Triangle (answers.Shapecolor, answers.Textcolor, answers.Text)
+            } else if (answers.Shape === "circle") {
+                newShape = new Circle (answers.Shapecolor, answers.Textcolor, answers.Text)
+            } else {
+                newShape = new Square (answers.Shapecolor, answers.Textcolor, answers.Text)
+            };
+            console.log (newShape.render());
+            fs.writeFile('logo.svg', newShape.render(),
+                (err) => {
+                    if (err)
+                        console.log(err);
+                    else {
+                        console.log("Generated logo.svg\n");
+                    }
+                })
         }
-      }))}
+        )
+}
 
 init();
